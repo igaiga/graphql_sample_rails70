1db1f654aa9760dd7302698cd2956ad629cc0936
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-04-24 11:12:51 +0900

    bin/rails g graphql:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
 end
