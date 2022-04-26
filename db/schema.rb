3d6957a40d8e817bbd7a984fdaff965557c0dc6e
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-04-26 10:24:30 +0900

    bin/rails g model book title user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_24_021037) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_011833) do
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_24_021037) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "users"
 end
