67bc33fe25b1cff9b98cfd71f11ac38885ba88ad
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-04-26 10:48:06 +0900

    bin/rails g graphql:object Book

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 module Types
-  class UserType < Types::BaseObject
+  class BookType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String, null: false
-    field :email, String
+    field :title, String, null: false
+    field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-
-    field :books, [BookType], null: false
   end
 end
